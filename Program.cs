71e9ae9ba22c214496ab2d06968c0863d581eacb
Joshua Kiley <joshua.kiley.dev@gmail.com>
2020-01-27 09:16:43 -0500

    Initiate dotnet C# program

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program initiated.");
+
+        }
+    }
+}
