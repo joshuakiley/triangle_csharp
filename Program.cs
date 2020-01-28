4bd2d3ef572d1d9a959c9a61bef9a2c1cf7e310e
Joshua Kiley <joshua.kiley.dev@gmail.com>
2020-01-28 00:09:32 -0500

    Figured out how to split the string into an array and convert strings to integers

--- Program.cs
+++ Program.cs
@@ -6,8 +6,29 @@ namespace triangle
     {
         static void Main(string[] args)
         {
-            string data = System.IO.File.ReadAllText("./data/sample.txt");
-            Console.WriteLine(data);
+            Console.WriteLine("Please enter the file name excluding the file extension:");
+
+            string fileName = Console.ReadLine();
+
+            string strData = System.IO.File.ReadAllText($"./data/{fileName}.txt");
+
+            string[] data = strData.Split(
+                new[] { "\r\n", "\r", "\n" },
+                StringSplitOptions.None
+            );
+
+            try
+            {
+                int m = Int32.Parse(data[0]);
+                Console.WriteLine(m + m);
+
+            }
+            catch (FormatException err)
+            {
+                Console.WriteLine($"Error: {err.Message}");
+            }
+
+
 
         }
     }
